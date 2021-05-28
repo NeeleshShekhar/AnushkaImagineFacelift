4a66190988591ea757944244a4ed6041370f46ed
NeeleshShekhar <52444240+NeeleshShekhar@users.noreply.github.com>
2021-05-28 17:28:18 +0530

    meta placed again back o to index.html

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ function App() {
     <div className="main">
       <Helmet>
       <title>SkilWil: Earn and learn</title>
-      <meta charset="utf-8" />
-      <link rel="icon" href="%PUBLIC_URL%/skilwilicon.png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <meta
-      name="description"
-      content="Intellect is the most underrated superpower!"
-      />
       <meta name="description" content="Earn while you learn ! Join your friends in solving brainstorming question from various subject and earn rewards. Also, ask your subject doubts for free." />
       <meta name="theme-color" content="#008f68" />
     </Helmet>
