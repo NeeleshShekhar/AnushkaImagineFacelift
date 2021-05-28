a2b1d620a73a9475f97124d5a2613dd44da45453
NeeleshShekhar <52444240+NeeleshShekhar@users.noreply.github.com>
2021-05-28 17:13:41 +0530

    Meta tags added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import MainComponent from './components/MainComponent';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="main">
-    
+      <Helmet>
+      <title>SkilWil: Earn and learn</title>
+      <meta name="description" content="Earn while you learn ! Join your friends in solving brainstorming question from various subject and earn rewards. Also, ask your subject doubts for free." />
+      <meta name="theme-color" content="#008f68" />
+    </Helmet>
       <MainComponent/>
     </div>
   );
