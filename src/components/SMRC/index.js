bf08ce21e14afea0ea345c4c04d57150ce8f3718
NeeleshShekhar <52444240+NeeleshShekhar@users.noreply.github.com>
2021-06-28 00:39:06 +0530

    Prep

--- src/components/SMRC/index.js
+++ src/components/SMRC/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, Button, NavItem, NavLink, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse,  Card, Button, CardHeader, CardFooter, CardBody,
+    CardTitle, CardText , NavItem, NavLink, Jumbotron } from 'reactstrap';
 import ReactGA from "react-ga";
 import * as ROUTES from '../../constants/routes';
 import smrc1 from '../../Images/smrc1.png';
 import smrc2 from '../../Images/smrc1.png';
+import ram from '../../Images/ram.jpeg';
 import { LaptopWindowsTwoTone } from "@material-ui/icons";
 
 
@@ -43,13 +45,45 @@ const SMRC = (props) => {
         </div>
         </div>
         <div className="row">
-        <div className="col-lg-4 col-sm-12">
-        
-        
+        <div className="col-lg-4 col-sm-12 ">
+        <div className="container " >
+        <Card className="smrc-event-card">
+        <CardHeader className="smrc-card-head">Event details</CardHeader>
+        <CardBody className="smrc-card-body">
+          <CardTitle tag="h5">Date:</CardTitle>
+          <CardText>3<sup>rd</sup> July, 2021</CardText>
+          <CardTitle tag="h5">Quiz Time</CardTitle>
+          <CardText>3:00 PM</CardText>
+          <CardTitle tag="h5">Prep TalkTime</CardTitle>
+          <CardText>3:00 PM</CardText>
+          
+          <a  href="https://docs.google.com/forms/d/e/1FAIpQLScnbl6zGfoUPCTIexdjc5r_xzJRsiCET37RWHSS4nWiEB55Ug/viewform?usp=sf_link" target="_blank"><Button className = "Register-smrchome"  style={{marginLeft:"2px"}}> Register for SMRC </Button>
+          </a>
+
+        </CardBody>
+  
+      </Card>
         </div>
         </div>
-        
+        <div className="col-lg-8 col-sm-12">
+        <div className="row">
+            <div className="col-lg-6 col-sm-12">
+              <div className="event-card">
+              <div className="event-smrc-head" >Prep Talk</div>
+              <div className="event-smrc-name" >Sagar Ramchandani</div>
+              <div className="event-smrc-namedesc">Student of University of Cologne, Germany </div>
+              <div className="event-smrc-namedesc">Alumini, VESAC Mumbai </div>
+                <br/>
+              <div className="event-smrc-desc">Building your portfolio for admission in college outside India </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-sm-12">
+              <img src={ram} className="smrc-photo" alt="Sagar Ramchandani"/>
+            </div>
         </div>
+        </div>
+        </div>
+      </div>
         <br/>
         
    </div>
