d2cfdab9f1628dc1f30486950e93c6a2c26c7d2a
NeeleshShekhar <52444240+NeeleshShekhar@users.noreply.github.com>
2021-06-27 21:07:38 +0530

    register button working

--- src/components/SMRC/index.js
+++ src/components/SMRC/index.js
@@ -3,6 +3,7 @@ import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, Button, NavItem, Nav
 import ReactGA from "react-ga";
 import * as ROUTES from '../../constants/routes';
 import smrc1 from '../../Images/smrc1.png';
+import smrc2 from '../../Images/smrc1.png';
 import { LaptopWindowsTwoTone } from "@material-ui/icons";
 
 
@@ -27,22 +28,30 @@ const SMRC = (props) => {
          <div className="container ">
         <div className="row">
         <div className="col-lg-7 col-sm-12">
-        <h1 className="smrc-head1">SMRC</h1>
+       <h1 className="smrc-head1">SMRC</h1>
         <h2 style={{color:"#064c5f"}}>Skilwil Mathematics Reward Contest </h2>
-        <div className="smrc-content">A splendid annual quiz contest – Previously known as M³, 
+      <div className="smrc-content">A splendid annual quiz contest – Previously known as M³, 
         which signifies a competition for the title of Math Muscle Man, 
-        is an hour long Math quiz contest organized by the department of VESAC.
+        is an hour long Math quiz contest organized by the department of VESAC. 
         
         </div>
         <div className="button-smrc">
-        <a  href="https://docs.google.com/forms/d/e/1FAIpQLScnbl6zGfoUPCTIexdjc5r_xzJRsiCET37RWHSS4nWiEB55Ug/viewform?usp=sf_link" target="_blank"><Button className = "Register-smrchome"  outline style={{marginLeft:"2px"}}> Register for SMRC </Button>
+        <a  href="https://docs.google.com/forms/d/e/1FAIpQLScnbl6zGfoUPCTIexdjc5r_xzJRsiCET37RWHSS4nWiEB55Ug/viewform?usp=sf_link" target="_blank"><Button className = "Register-smrchome"  style={{marginLeft:"2px"}}> Register for SMRC </Button>
         </a></div></div>
         <div className="col-lg-5  col-sm-12">
         <img src={smrc1} className="imagetop" alt="Marketing"/>
         </div>
         </div>
+        <div className="row">
+        <div className="col-lg-4 col-sm-12">
+        
+        
+        </div>
+        </div>
+        
         </div>
         <br/>
+        
    </div>
 
     );
